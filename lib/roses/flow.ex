3cd5ce728327a5c28f3234bf45fbb42b83178132
Daniel Morandini <danielmorandini@me.com>
2022-04-22 18:01:00 +0200

    Add possibility to configure flow execution

--- lib/roses/flow.ex
+++ lib/roses/flow.ex
@@ -4,9 +4,9 @@ defmodule Roses.Flow do
   entry in the enumerable is expected to be a String. Words are tokenized
   splitting the line in whitespaces.
   """
-  def from_enumerables(enumerables) do
+  def from_enumerables(enumerables, opts \\ []) do
     enumerables
-    |> Flow.from_enumerables()
+    |> Flow.from_enumerables(opts)
     |> Flow.flat_map(&extract/1)
     |> Flow.partition()
     |> Flow.reduce(fn -> %{} end, fn word, acc ->
